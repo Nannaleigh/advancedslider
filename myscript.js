8397d655597dc5da38c3b213ee1d20c7b1c5455d
Nannaleigh <tatianna.martinez71@gmail.com>
2022-12-30 10:37:57 -0700

    first commit - installed flexslider

--- myscript.js
+++ myscript.js
@@ -1,3 +1,9 @@
-$(window).load(function(){
-    $('.flexslider').flexslider();
-})
+$(window).on("load", function () {
+  $(".flexslider").flexslider({
+    animation: "slide",
+    slideShowSpeed: 2000,
+    direction: "vertical",
+    reverse: true,
+    pauseOnHover: true,
+  });
+});
